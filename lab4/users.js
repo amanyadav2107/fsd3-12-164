7ea85313c3c6aea2c91349e93c877d9fea1d6545
Aman Yadav <amanyadavchak@gmail.com>
2026-09-12 09:23:01 +0530

    Lab 4

---
+++ lab4/users.js
@@ -0,0 +1,9 @@
+//We use in memory database
+let users =[
+    {id:1,name:'Aman Yadav', mob:'46545xxxxx', email:'aman.example@gmail.com'},
+    {id:1,name:'AmeyYadav', mob:'46885xxxxx', email:'amey.example@gmail.com'},
+]
+
+let nextId =3;
+
+export const getUsers =() => users; //export is used to import somewhere else
