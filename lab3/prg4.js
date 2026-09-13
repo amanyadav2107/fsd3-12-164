5d63f7ac74ab393b1a14e2fc98d7836db52b880d
Aman Yadav <amanyadavchak@gmail.com>
2026-09-13 09:24:32 +0530

    Prg 4

--- lab3/prg4.js
+++ lab3/prg4.js
@@ -1,32 +1,24 @@
-import http from 'http';
+import http from "http";
 
- const server = http.createServer((req,res) => {
-     res.writeHead(200,{'conttent-type': 'text/json'});
-     console.log("Client URL;",req.url);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/json" });
+  console.log("Client ULR:", req.url);
 
-
-     const product =[{
-        name: "Iphone",
-        price: 85000,
-        qty:2,
-        discount: 15,
-     },
-     {
-      name: "laptop",
-      price: 155000,
-      qty:1,
+  const product = [
+    {
+      name: "Iphone",
+      price: 85000,
+      qty: 2,
+      discount: 15,
+    },
+    {
+      name: "HP Laptop",
+      price: 95000,
+      qty: 1,
       discount: 25,
-     },
-     {
-        name: "Mouse",
-        price: 2500,
-        qty:2,
-        discount: 20,
-     },
-    ];
+    },
+  ];
+  res.end(JSON.stringify(product));
+});
 
-     res.end(JSON.stringify(product));
- });
-
-
- server.listen(3000,() => console.log("prg4 is runnig at 3000...."));
+server.listen(3000, () => console.log("prg4 is running at 3000..."));
