3433dd3a2870f6c32dc0e56630fa759215227b51
Aman Yadav <amanyadavchak@gmail.com>
2026-09-17 12:16:17 +0530

    Prg 7

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,14 +1,27 @@
 import http from "http";
-import{ getUsers} from "./users.js";
+import{ getUsers,addUser} from "./users.js";
 
 const server = http.createServer((req, res) => {
   if ((req.url === "/api/users"&& req.method === "GET")) {
     res.end(JSON.stringify(getUsers()));
   }
   else if ((req.url === "/api/users"&& req.method === "POST")) {
-    res.end(JSON.stringify({ msg: "add user" }));
+    let body =' ';
+    req.on('data',(chunk)=>{
+      body += chunk;
+
+    })
+    req.on('end',()=>{
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({msg:"user added", userCreated}));
+
+    })
+
+
   } 
   else if ((req.url === "/api/users/1"&& req.method === "GET")) {
+    
     res.end(JSON.stringify({ msg: "single user with id 1" }));
   } 
   else if ((req.url === "/api/users/1"&& req.method === "PUT")) {
